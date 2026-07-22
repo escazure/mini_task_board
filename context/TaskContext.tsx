08ec7c8ad450fc13cb6687c6efd00a3004a20c99
escazure <haiper.1453@gmail.com>
2026-07-22 09:59:12 +0200

    Added context and initial tasks

---
+++ context/TaskContext.tsx
@@ -0,0 +1,27 @@
+"use client";
+
+import { createContext } from "react";
+import { Task } from "@/types/task";
+
+const initialTasks: Task[] = [
+  { id: 1, title: "Task 1", description: "Donec maximus ex nec neque.", status: "done" },
+  { id: 2, title: "Task 2", description: "Lorem ipsum dolor sit amet.", status: "in-progress" },
+  { id: 3, title: "Task 3", description: "In vestibulum diam vitae congue.", status: "todo" },
+  { id: 4, title: "Task 4", description: "Etiam sollicitudin massa sapien. Sed.", status: "todo" },
+  { id: 5, title: "Task 5", description: "Mauris id congue sem, at.", status: "todo" },
+  { id: 6, title: "Task 6", description: "Nulla ac leo ut nunc.", status: "todo" },
+];
+
+interface TaskContextType {
+    tasks: Task[];
+}
+
+const TaskContext = createContext<TaskContextType | undefined>(undefined);
+
+export function TaskProvider({ children }: { children: React.ReactNode}){
+    return (
+        <TaskContext.Provider value={{ tasks: initialTasks }}>
+            {children}
+        </TaskContext.Provider>
+    );
+}
