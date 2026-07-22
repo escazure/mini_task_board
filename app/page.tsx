052b1bb091b007f0b5226f067fd4c49a6e341dac
escazure <haiper.1453@gmail.com>
2026-07-22 10:24:30 +0200

    Added filter for tasks to the main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,40 @@
 "use client";
 
-import { useContext } from "react";
+import { useContext, useState } from "react";
 import { TaskContext } from "@/context/TaskContext";
+import { TaskStatus } from "@/types/task";
 
 export default function HomePage(){
     const context = useContext(TaskContext);
+    const [filter, setFilter] = useState<TaskStatus | "all">("all");
+    const filterOptions = ["all", "todo", "in-progress", "done"];
 
     if(!context){
         return <p>TaskContext is missing</p>;
     }
 
+    const filteredTasks = context.tasks.filter((task) => {
+        if (filter === "all") return true;
+        return task.status === filter;
+    });
+
     return (
-        <ul>
-            {context.tasks.map((task) => (
-                <li key={task.id}><strong>{task.title}</strong>: {task.description} <strong>{task.status}</strong></li>
-            ))}
-        </ul>
+        <div>
+            <div style={{ marginBottom: "16px", display: "flex", gap: "8px" }}>
+                {filterOptions.map((filterOption) => (
+                    <button 
+                        key={filterOption} 
+                        onClick={() => setFilter(filterOption as TaskStatus | "all")}
+                        style={{ fontWeight: filter === filterOption ? "bold" : "normal"}}>
+                        {filterOption}
+                    </button>
+                ))}
+            </div>
+            <ul>
+                {filteredTasks.map((task) => (
+                    <li key={task.id}><strong>{task.title}</strong>: {task.description} <strong>{task.status}</strong></li>
+                ))}
+            </ul>
+        </div>
     );
 }
